667b4141e03b4e32caae59afec7f99997b4fb9c5
Richard Szeto <richardszeto74@gmail.com>
2018-08-17 18:19:22 -0700

    Added more unit tests

--- UnitTests/QueueUnitTest/QueueUnitTest.cpp
+++ UnitTests/QueueUnitTest/QueueUnitTest.cpp
@@ -9,21 +9,101 @@
 #include "QueueUnitTest.hpp"
 
 TEST(Queue, Enqueue) {
-    
+    Queue<int> queue;
+    EXPECT_EQ(queue.getCount(), 0);
+    queue.enqueue(1);
+    EXPECT_EQ(queue.getCount(), 1);
+    queue.enqueue(2);
+    EXPECT_EQ(queue.getCount(), 2);
+    queue.enqueue(3);
+    EXPECT_EQ(queue.getCount(), 3);
+    EXPECT_EQ(queue.top(), 1);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 2);
+    EXPECT_EQ(queue.top(), 2);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 1);
+    EXPECT_EQ(queue.top(), 3);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 0);
 }
 
 TEST(Queue, Top) {
-    
+    Queue<int> queue;
+    EXPECT_EQ(queue.getCount(), 0);
+    queue.enqueue(1);
+    EXPECT_EQ(queue.getCount(), 1);
+    queue.enqueue(2);
+    EXPECT_EQ(queue.getCount(), 2);
+    queue.enqueue(3);
+    EXPECT_EQ(queue.getCount(), 3);
+    EXPECT_EQ(queue.top(), 1);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 2);
+    EXPECT_EQ(queue.top(), 2);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 1);
+    EXPECT_EQ(queue.top(), 3);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 0);
 }
 
 TEST(Queue, Dequeue) {
-    
+    Queue<int> queue;
+    EXPECT_EQ(queue.getCount(), 0);
+    queue.enqueue(1);
+    EXPECT_EQ(queue.getCount(), 1);
+    queue.enqueue(2);
+    EXPECT_EQ(queue.getCount(), 2);
+    queue.enqueue(3);
+    EXPECT_EQ(queue.getCount(), 3);
+    EXPECT_EQ(queue.top(), 1);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 2);
+    EXPECT_EQ(queue.top(), 2);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 1);
+    EXPECT_EQ(queue.top(), 3);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 0);
 }
 
 TEST(Queue, GetCount) {
-    
+    Queue<int> queue;
+    EXPECT_EQ(queue.getCount(), 0);
+    queue.enqueue(1);
+    EXPECT_EQ(queue.getCount(), 1);
+    queue.enqueue(2);
+    EXPECT_EQ(queue.getCount(), 2);
+    queue.enqueue(3);
+    EXPECT_EQ(queue.getCount(), 3);
+    EXPECT_EQ(queue.top(), 1);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 2);
+    EXPECT_EQ(queue.top(), 2);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 1);
+    EXPECT_EQ(queue.top(), 3);
+    queue.dequeue();
+    EXPECT_EQ(queue.getCount(), 0);
 }
 
 TEST(Queue, IsEmpty) {
-    
+    Queue<int> queue;
+    EXPECT_TRUE(queue.isEmpty());
+    queue.enqueue(1);
+    EXPECT_FALSE(queue.isEmpty());
+    queue.enqueue(2);
+    EXPECT_FALSE(queue.isEmpty());
+    queue.enqueue(3);
+    EXPECT_FALSE(queue.isEmpty());
+    EXPECT_EQ(queue.top(), 1);
+    queue.dequeue();
+    EXPECT_FALSE(queue.isEmpty());
+    EXPECT_EQ(queue.top(), 2);
+    queue.dequeue();
+    EXPECT_FALSE(queue.isEmpty());
+    EXPECT_EQ(queue.top(), 3);
+    queue.dequeue();
+    EXPECT_TRUE(queue.isEmpty());
 }
--- UnitTests/StackUnitTest/StackUnitTest.cpp
+++ UnitTests/StackUnitTest/StackUnitTest.cpp
@@ -9,21 +9,97 @@
 #include "StackUnitTest.hpp"
 
 TEST(Stack, Push) {
-    
+    Stack<int> stack;
+    EXPECT_EQ(stack.getCount(), 0);
+    stack.push(1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
+    stack.push(2);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.push(3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_EQ(stack.getCount(), 3);
 }
 
 TEST(Stack, Top) {
-    
+    Stack<int> stack;
+    EXPECT_EQ(stack.getCount(), 0);
+    stack.push(1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
+    stack.push(2);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.push(3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_EQ(stack.getCount(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
 }
 
 TEST(Stack, Pop) {
-    
+    Stack<int> stack;
+    EXPECT_EQ(stack.getCount(), 0);
+    stack.push(1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
+    stack.push(2);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.push(3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_EQ(stack.getCount(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
 }
 
 TEST(Stack, GetCount) {
-    
+    Stack<int> stack;
+    EXPECT_EQ(stack.getCount(), 0);
+    stack.push(1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
+    stack.push(2);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.push(3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_EQ(stack.getCount(), 3);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_EQ(stack.getCount(), 2);
+    stack.pop();
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_EQ(stack.getCount(), 1);
 }
 
 TEST(Stack, IsEmpty) {
-    
+    Stack<int> stack;
+    EXPECT_TRUE(stack.isEmpty());
+    stack.push(1);
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.push(2);
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.push(3);
+    EXPECT_EQ(stack.top(), 3);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.pop();
+    EXPECT_EQ(stack.top(), 2);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.pop();
+    EXPECT_EQ(stack.top(), 1);
+    EXPECT_FALSE(stack.isEmpty());
+    stack.pop();
+    EXPECT_TRUE(stack.isEmpty());
 }
